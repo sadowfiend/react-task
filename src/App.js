55bbce426de40ae0e2f74376af8247a2e2e7b85f
Aziz <atamirzaev.aziz@mail.ru>
2022-03-18 16:53:35 +0600

    fetched data from jsonplaceholder and created all methods to render users

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
+import User from "./components/User";
+import AddUser from "./components/AddUser";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [users, setUsers] = useState([])
+    useEffect(() => {
+        fetchData()
+    }, [])
+
+    const fetchData = async () => {
+        await fetch('https://jsonplaceholder.typicode.com/users')
+            .then(res => res.json())
+            .then(data => setUsers(data))
+            .catch((err) => {
+                console.log(err)
+            })
+    }
+
+    const onAdd = async (name) => {
+        await fetch('https://jsonplaceholder.typicode.com/users', {
+            method: 'POST',
+            body: JSON.stringify({
+                name: name
+            }),
+            headers: {
+                "Content-type": "application/json; charset=UTF-8",
+            }
+        })
+            .then((res) => {
+                if (res.status !== 201) {
+                    return
+                } else {
+                    return res.json();
+                }
+            })
+            .then((data) => {
+                setUsers((users) => [...users, data])
+            })
+            .catch((err) => {
+                console.log(err)
+            })
+    }
+
+    const onDelete = async (id) => {
+        await fetch(`https://jsonplaceholder.typicode.com/users/${id}`, {
+            method: 'DELETE'
+        })
+            .then((res) => {
+                if (res.status !== 200) {
+                    return
+                } else {
+                    setUsers(users.filter((user) => {
+                        return user.id !== id
+                    }))
+                }
+            })
+            .catch((err) => {
+                console.log(err)
+            })
+    }
+
+    return (
+        <div className="App">
+            <AddUser onAdd={onAdd}/>
+            <div className="container">
+                {
+                    users.map((user) => {
+                        return <User
+                            id={user.id}
+                            key={user.id}
+                            name={user.name}
+                            onDelete={onDelete}
+                        />
+                    })
+                }
+            </div>
+        </div>
+    );
 }
 
 export default App;
